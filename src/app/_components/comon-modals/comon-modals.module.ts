19b5399c34234acf4b45e35a5a7fded349476de8
aykutcant <aykut777@protonmail.com>
2019-12-25 09:10:46 +0300

    share modal created

---
+++ src/app/_components/comon-modals/comon-modals.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlertModalComponent } from './alert-modal/alert-modal.component';
+
+
+
+@NgModule({
+  declarations: [AlertModalComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComonModalsModule { }
